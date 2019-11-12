02d6620c9cf36607c9a5ebe8eeb3de0d4e7558cd
YuningYao <812559544@qq.com>
2019-11-12 10:06:38 +0800

    add gif maker

--- tools/make_gif.js
+++ tools/make_gif.js
@@ -1,8 +1,13 @@
 const pngFileStream = require('png-file-stream');
 const GIFEncoder = require('gifencoder');
 const fs = require('fs');
-const encoder = new GIFEncoder(2062, 1378);
 
-pngFileStream('source/_drafts/聊聊jvm/vmstack_?.png')
-  .pipe(encoder.createWriteStream({ repeat: -1, delay: 500, quality: 60 }))
-  .pipe(fs.createWriteStream('myanimated.gif'));
+const { getArg, mustHava } = require('./argv');
+mustHava(['--origin', '--target', '--height', '--width']);
+
+const encoder = new GIFEncoder(Number(getArg('--width')), Number(getArg('--height')));
+
+console.log(getArg('--origin'))
+pngFileStream(getArg('--origin'))
+  .pipe(encoder.createWriteStream({ repeat: -1, delay: Number(getArg('--delay') || 500), quality: 100 }))
+  .pipe(fs.createWriteStream(getArg('--target')));
